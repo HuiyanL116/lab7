ad439592a11393ed156edae16870545fca6c3e92
Huiyan(Morgan) Li <huiyaniani@gmail.com>
2021-02-19 03:17:59 +0800

    lab7 in class 1

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,12 @@ var projects = require('../projects.json');
  * GET home page.
  */
 
-exports.view = function(req, res){
+exports.view = function(request, response){
 	data['viewAlt'] = true;
 	response.render('index', projects);
 };
 
-exports.viewAlt = function(req, res){
+exports.viewAlt = function(request, response){
 	data['viewAlt'] = false;
 	response.render('index', projects);
 }
